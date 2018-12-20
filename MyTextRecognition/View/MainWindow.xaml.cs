01cfdcf074361afbbebd9048e449de6cc537e2bb
Sebastian Strubert <sebi@strubert.de>
2018-12-20 15:10:53 +0100

    MVVM structure and BaseViewModel implmentation

--- MyTextRecognition/View/MainWindow.xaml.cs
+++ MyTextRecognition/View/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace MyTextRecognition
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ MyTextRecognition/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextRecognition
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
